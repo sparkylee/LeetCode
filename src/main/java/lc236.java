4799dec575e1d2ef8162463c6e8142d371e8f48a
sparky <yaoqiang.li.uw@gmail.com>
2019-05-19 23:10:39 +0800

    finish 236

--- src/main/java/lc236.java
+++ src/main/java/lc236.java
@@ -62,11 +62,38 @@ public class lc236 {
         return linkTNs(treeNodes);
     }
 
+    @Test
+    public void test1() {
+        Integer[] nums = {3, 5, 1, 6, 2, 0, 8, null, null, 7, 4};
+        test(nums, 1, 2);
+    }
+
+    @Test
+    public void test2() {
+        Integer[] nums = {3, 5, 1, 6, 2, 0, 8, null, null, 7, 4};
+        test(nums, 1, 10);
+    }
+
+    @Test
+    public void test3() {
+        Integer[] nums = {3, 5, 1, 6, 2, 0, 8, null, null, 7, 4};
+        test(nums, 0, 0);
+    }
+
+    private void test(Integer[] nums, int pi, int qi) {
+        TreeNode[] tnArray = createTNs(nums);
+        TreeNode root = linkTNs(tnArray);
+        TreeNode p = tnArray[pi];
+        TreeNode q = tnArray[qi];
+        TreeNode x = lowestCommonAncestor(root, p, q);
+        System.out.println(p.val + " " + q.val + " " + x.val);
+    }
+
+    ///////////////////////////////////////////
     class Record {
         TreeNode node;
         boolean left;
         boolean right;
-
         Record(TreeNode node, boolean left, boolean right) {
             this.node = node;
             this.left = left;
@@ -74,40 +101,58 @@ public class lc236 {
         }
     }
 
-    List<Record> records = new ArrayList<>();
 
-    private boolean search4Node(TreeNode root, TreeNode tn, boolean left, boolean right) {
-        Record r = new Record(root, false, false);
-        if (root == tn)
-            return true;
+    private boolean search4Node(TreeNode root, TreeNode tn) {
+        if (root == tn) return true;
+        if (root == null) return false;
+        return (search4Node(root.left, tn) || search4Node(root.right, tn));
+    }
 
-        if (root == null)
+    private boolean searchFirstNode(TreeNode root, TreeNode tn, List<Record> records) {
+        if (root == null || tn == null)
             return false;
+        Record r = new Record(root, false, false);
+        if (root == tn) {
+            r.left = false;
+            r.right = false;
+            records.add(r);
+            return true;
+        }
+        r.left = true;
+        r.right = false;
         records.add(r);
-        if (left) {
-            boolean found = search4Node(root.left, tn, true, true);
-            if (found)
-                return true;
-        }
-        if (right) {
-            boolean found = search4Node(root.right, tn, true, true);
-            if (found) return found;
-        }
+        if (searchFirstNode(root.left, tn, records))
+            return true;
+        r.left = false;
+        r.right = true;
+        if (searchFirstNode(root.right, tn, records))
+            return true;
+        records.remove(records.size() - 1);
         return false;
     }
 
-    private TreeNode search4AnotherNode(TreeNode root, TreeNode tn, boolean left, boolean right) {
+    private TreeNode search4AnotherNode(TreeNode tn, final List<Record> records) {
         for (int i = records.size() - 1; i >= 0; i--) {
             Record r = records.get(i);
-            boolean found = search4Node(r.node, tn, !r.left, !r.right);
-            if (found)
+            if (r.node == tn)
                 return r.node;
+            if (!r.left) {
+                boolean isFound = search4Node(r.node.left, tn);
+                if (isFound) return r.node;
+            }
+            if (!r.right) {
+                boolean isFound = search4Node(r.node.right, tn);
+                if (isFound) return r.node;
+            }
         }
         return null;
     }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        return null;
+        List<Record> records = new ArrayList<>();
+        searchFirstNode(root, p, records);
+        TreeNode x = search4AnotherNode(q, records);
+        return x;
     }
 
 }
