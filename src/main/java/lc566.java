fe72ec136e3f8ede9ec0e58a3da8db0adba87e9f
sparky <yaoqiang.li.uw@gmail.com>
2019-06-29 19:01:20 +0800

    test for lc566

--- src/main/java/lc566.java
+++ src/main/java/lc566.java
@@ -3,8 +3,27 @@ import org.junit.Test;
 public class lc566 {
     @Test
     public void test() {
+        t(new int[][]{{1, 2}, {3, 4}}, 1, 4);
+        t(new int[][]{{1, 2}, {3, 4}}, 2, 4);
     }
 
+    private void t(int[][] nums, int r, int c) {
+        printArray(nums);
+        int[][] m = matrixReshape(nums, r, c);
+        printArray(m);
+        System.out.println("done");
+    }
+
+    private void printArray(int[][] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int[] row = nums[i];
+            for (int j = 0; j < row.length; j++) {
+                System.out.print(row[j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
     public int[][] matrixReshape(int[][] nums, int r, int c) {
         int[][] m = new int[r][c];
         int ri = 0, ci = 0;
