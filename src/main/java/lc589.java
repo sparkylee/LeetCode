cb66bf90367e678440016ac97d574f0dd254dcbf
sparky <yaoqiang.li.uw@gmail.com>
2019-06-29 19:43:37 +0800

    complete lc589

---
+++ src/main/java/lc589.java
@@ -0,0 +1,43 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class lc589 {
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    ;
+
+    @Test
+    public void test() {
+
+    }
+
+    public void preorder(Node root, List<Integer> list) {
+        if (root == null) return;
+        list.add(root.val);
+        for (Node n : root.children) {
+            preorder(n, list);
+        }
+    }
+
+    public List<Integer> preorder(Node root) {
+        List<Integer> list = new ArrayList<>();
+        preorder(root, list);
+        return list;
+    }
+
+}
