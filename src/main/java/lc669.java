0ee6ca551d4e9a38178b26510532bfcb8bf3ebed
sparky <yaoqiang.li.uw@gmail.com>
2019-07-03 23:00:10 +0800

    bugfix and complete for lc669

--- src/main/java/lc669.java
+++ src/main/java/lc669.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+
 public class lc669 {
     public class TreeNode {
         int val;
@@ -11,6 +13,17 @@ public class lc669 {
 
     TreeNode TN(int x) {
         return new TreeNode(x);
+    }
+
+    @Test
+    public void test() {
+        TreeNode tn3 = TN(3), tn0 = TN(0), tn2 = TN(2), tn1 = TN(1), tn4 = TN(4);
+        tn3.left = tn0;
+        tn0.right = tn2;
+        tn3.right = tn4;
+        tn2.left = tn1;
+        TreeNode root = trimBST(tn3, 1, 3);
+        System.out.print(root);
     }
 
     TreeNode trimRoot(TreeNode root, int L, int R) {
@@ -42,14 +55,14 @@ public class lc669 {
             while (root.right != null && root.right.val > R) {
                 root.right = root.right.left;
             }
-            root = root.left;
+            root = root.right;
         }
     }
 
     public TreeNode trimBST(TreeNode root, int L, int R) {
         root = trimRoot(root, L, R);
         trimLeft(root, L);
-        trimLeft(root, R);
+        trimRight(root, R);
         return root;
     }
 }
