75372b59aaaa334a11ce5c47d63e797465ade36d
sparky <yaoqiang.li.uw@gmail.com>
2019-07-06 23:50:35 +0800

    fine tune for lc686

--- src/main/java/lc686.java
+++ src/main/java/lc686.java
@@ -1,15 +1,22 @@
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class lc686 {
+    @Test
+    public void t1() {
+        int[] result = shiftIndex("abcax");
+        System.out.println("dfafsd");
+    }
 
     private int[] shiftIndex(String s) {
         int[] shift = new int[s.length()];
         for (int i = 0; i < shift.length; i++)
             shift[i] = -1;
-        for (int i = 0; i < s.length(); i++) {
-            int j = i + 1;
-            while (j < s.length()) {
+        for (int i = 1; i < s.length(); i++) {
+            int j = 0;
+            while ((j + i) < s.length()) {
                 if (s.charAt(j + i) == s.charAt(j)) {
                     if (shift[i + j] == -1)
                         shift[i + j] = i;
@@ -22,6 +29,7 @@ public class lc686 {
     }
 
     private int matchSubstring(String s, String t) {
+        int[] shift = shiftIndex(s);
         return -1;
     }
 
