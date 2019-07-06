cd1aede1ed89da1e0d8c5d8ebd359f172a060ee1
sparky <yaoqiang.li.uw@gmail.com>
2019-07-07 00:30:08 +0800

    fix the shift value calculation for lc686

--- src/main/java/lc686.java
+++ src/main/java/lc686.java
@@ -7,13 +7,15 @@ public class lc686 {
     @Test
     public void t1() {
         int[] result = shiftIndex("abcax");
+        int i = matchSubstring("axa", "abaxcaxadt");
+        System.out.println(i);
         System.out.println("dfafsd");
     }
 
     private int[] shiftIndex(String s) {
         int[] shift = new int[s.length()];
-        for (int i = 0; i < shift.length; i++)
-            shift[i] = -1;
+        for (int j = 0; j < shift.length; j++)
+            shift[j] = -1;
         for (int i = 1; i < s.length(); i++) {
             int j = 0;
             while ((j + i) < s.length()) {
@@ -30,6 +32,24 @@ public class lc686 {
 
     private int matchSubstring(String s, String t) {
         int[] shift = shiftIndex(s);
+        int i = 0;
+        boolean isMatched = true;
+        while (i + s.length() <= t.length()) {
+            isMatched = true;
+            if (s.charAt(0) != t.charAt(i)) {
+                i++;
+                continue;
+            }
+            for (int j = 1; j < s.length(); j++) {
+                if (s.charAt(j) != t.charAt(i + j)) {
+                    i += shift[j] == -1 ? j : shift[j];
+                    isMatched = false;
+                    break;
+                }
+            }
+            if (isMatched)
+                return i;
+        }
         return -1;
     }
 
