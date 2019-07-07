c5a34c15a2e7d84cdf68b64f7e3218507249ad71
sparky <yaoqiang.li.uw@gmail.com>
2019-07-07 22:42:44 +0800

    init commit for lc700

---
+++ src/main/java/lc700.java
@@ -0,0 +1,19 @@
+public class lc700 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val) return root;
+        if (root.val < val)
+            return searchBST(root.right, val);
+        return searchBST(root.left, val);
+    }
+
+}
