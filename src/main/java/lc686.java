c3e01ac0001a0e0bee789e06bcc5cf377042583e
sparky <yaoqiang.li.uw@gmail.com>
2019-07-07 13:53:42 +0800

    re-organize calculation of next i,j for lc686

--- src/main/java/lc686.java
+++ src/main/java/lc686.java
@@ -14,12 +14,12 @@ public class lc686 {
 
     private int[] shiftIndex(String s) {
         int[] shift = new int[s.length()];
-        for (int j = 0; j < shift.length; j++)
-            shift[j] = -1;
+        for (int i = 0; i < shift.length; i++)
+            shift[i] = -1;
         for (int i = 1; i < s.length(); i++) {
             int j = 0;
             while ((j + i) < s.length()) {
-                if (s.charAt(j + i) == s.charAt(j)) {
+                if (s.charAt(i + j) == s.charAt(j)) {
                     if (shift[i + j] == -1)
                         shift[i + j] = i;
                     j++;
@@ -32,17 +32,24 @@ public class lc686 {
 
     private int matchSubstring(String s, String t) {
         int[] shift = shiftIndex(s);
-        int i = 0;
+        int i = 0, j = 0;
         boolean isMatched = true;
+        int next_i = 0;
+        int next_j = 0;
         while (i + s.length() <= t.length()) {
             isMatched = true;
-            if (s.charAt(0) != t.charAt(i)) {
-                i++;
-                continue;
-            }
-            for (int j = 1; j < s.length(); j++) {
+            i = next_i;
+            j = next_j;
+            for (; j < s.length(); j++) {
                 if (s.charAt(j) != t.charAt(i + j)) {
-                    i += shift[j] == -1 ? j : shift[j];
+                    if (j <= 2) {
+                        next_i = i + 1;
+                        next_j = 0;
+                    } else {
+                        int delta = shift[j - 1] == -1 ? j : shift[j];
+                        next_i = i + delta;
+                        next_j = j - delta;
+                    }
                     isMatched = false;
                     break;
                 }
