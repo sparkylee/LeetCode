a01216504ce78394748b0458fdb683f10ff69f62
sparky <yaoqiang.li.uw@gmail.com>
2019-07-07 18:18:18 +0800

    complete  lc693

---
+++ src/main/java/lc696.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+
+public class lc696 {
+
+    @Test
+    public void test() {
+        t("00110011");
+        t("");
+        t("01");
+        t("0");
+        t("1");
+        t("11");
+        t("10101");
+    }
+
+    private void t(String s) {
+        System.out.println(countBinarySubstrings(s));
+    }
+
+    public int countBinarySubstrings(String s) {
+        int k = 0;
+        int count = 0;
+        while (k < s.length()) {
+            while (k + 1 < s.length() && s.charAt(k) == s.charAt(k + 1))
+                k++;
+            int i = 0;
+            while ((k - i) >= 0 && (k + 1 + i) < s.length()
+                    && s.charAt(k - i) == s.charAt(k)
+                    && s.charAt(k + 1 + i) == s.charAt(k + 1)) {
+                i++;
+                count++;
+            }
+            k++;
+        }
+        return count;
+    }
+}
