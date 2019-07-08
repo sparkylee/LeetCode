7c2f14b5a59c443b53ac773b37f48e878b145326
sparky <yaoqiang.li.uw@gmail.com>
2019-07-08 22:11:25 +0800

    swithc the meaning of input val in the longest path complete lc687

--- src/main/java/lc687.java
+++ src/main/java/lc687.java
@@ -11,25 +11,20 @@ public class lc687 {
 
     int count = 0;
 
-    private int longestPath(TreeNode root, int val) {
-        if (root == null) return -1;
-        if (root.val != val) // new root
-        {
-            longestUnivaluePath(root);
-            return -1;
-        }
-        int lv = longestPath(root.left, val);
-        int rv = longestPath(root.right, val);
-        int v = 0;
-        if (lv >= 0) v += lv + 1;
-        if (rv >= 0) v += rv + 1;
-        return v;
+    private int longestPath(TreeNode root, Integer val) {
+        if (root == null) return 0;
+        int lv = longestPath(root.left, root.val);
+        int rv = longestPath(root.right, root.val);
+        int v = lv + rv;
+        this.count = Math.max(v, this.count);
+        if (val != null && root.val == val) //
+            return Math.max(lv, rv) + 1;
+        return 0;
     }
 
     public int longestUnivaluePath(TreeNode root) {
         if (root == null) return 0;
-        int v = longestPath(root, root.val);
-        this.count = Math.max(v, this.count);
+        longestPath(root, null);
         return this.count;
     }
 
