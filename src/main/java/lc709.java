583c2b8f1f315376d6de1560edf38f8da313662e
sparky <yaoqiang.li.uw@gmail.com>
2019-07-08 22:48:27 +0800

    fix a typo and add test

--- src/main/java/lc709.java
+++ src/main/java/lc709.java
@@ -1,4 +1,13 @@
+import org.junit.Test;
+
 public class lc709 {
+    @Test
+    public void test() {
+        System.out.println(toLowerCase("SSDA"));
+        System.out.println(toLowerCase(""));
+        System.out.println(toLowerCase("xa2A"));
+    }
+
     public String toLowerCase(String str) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
@@ -6,7 +15,7 @@ public class lc709 {
             if (c >= 'A' && c <= 'Z') {
                 c = (char) ((c - 'A') + 'a');
             }
-            sb.append(sb);
+            sb.append(c);
         }
         return sb.toString();
     }
