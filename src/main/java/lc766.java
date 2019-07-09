af20ff6cbf8bd2a30d910012f915ecc59514f154
sparky <yaoqiang.li.uw@gmail.com>
2019-07-09 22:56:11 +0800

    init commit for lc766

---
+++ src/main/java/lc766.java
@@ -0,0 +1,14 @@
+public class lc766 {
+
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int i = 0, j = 0;
+        while (i < matrix.length && j < matrix[i].length) {
+            if (matrix[i][j] != matrix[0][0])
+                return false;
+            i++;
+            j++;
+        }
+        return false;
+    }
+
+}
