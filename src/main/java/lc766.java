3feb30cabf4612443cfec7f16f03169eb6c2056d
sparky <yaoqiang.li.uw@gmail.com>
2019-07-09 23:01:20 +0800

    fit the topic requirement

--- src/main/java/lc766.java
+++ src/main/java/lc766.java
@@ -2,11 +2,15 @@ public class lc766 {
 
     public boolean isToeplitzMatrix(int[][] matrix) {
         int i = 0, j = 0;
-        while (i < matrix.length && j < matrix[i].length) {
-            if (matrix[i][j] != matrix[0][0])
-                return false;
-            i++;
-            j++;
+        for (int ii = 0; ii < matrix.length; ii++) {
+            i = ii;
+            j = 0;
+            while (i < matrix.length && j < matrix[i].length) {
+                if (matrix[i][j] != matrix[ii][0])
+                    return false;
+                i++;
+                j++;
+            }
         }
         return true;
     }
