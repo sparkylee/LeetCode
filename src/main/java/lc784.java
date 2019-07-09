187d0ae017688d04eec36323e2b889a851be4645
sparky <yaoqiang.li.uw@gmail.com>
2019-07-09 22:05:27 +0800

    init commit for lc784

---
+++ src/main/java/lc784.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class lc784 {
+
+    private char toggle(char x) {
+        if (x >= 'A' && x <= 'Z') x = (char) (x - 'A' + 'a');
+        if (x >= 'a' && x <= 'z') x = (char) (x - 'a' + 'A');
+        return x;
+    }
+
+    private String permute(String s, int i) {
+        char x = s.charAt(i);
+        x = toggle(x);
+        return s.substring(0, i) + x + s.substring(i + 1, s.length() - 1);
+    }
+
+    public List<String> letterCasePermutation(String S) {
+        List<String> list = new ArrayList<>();
+        if (S == null || S.length() == 0)
+            return list;
+        list.add(S);
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            if (c >= '0' && c <= '9')
+                continue;
+            int len = list.size();
+            for (int j = 0; j < len; j++) {
+                String x = permute(list.get(j), i);
+                list.add(x);
+            }
+        }
+        return list;
+    }
+
+}
