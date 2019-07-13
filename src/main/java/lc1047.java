a83231435016441c57c663df5d178406d067aecf
sparky <yaoqiang.li.uw@gmail.com>
2019-07-13 23:58:11 +0800

    bug fix for over boundary

--- src/main/java/lc1047.java
+++ src/main/java/lc1047.java
@@ -3,7 +3,7 @@ public class lc1047 {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < S.length(); i++) {
             char c = S.charAt(i);
-            if (sb.charAt(sb.length() - 1) == c)
+            if (sb.length() > 0 && sb.charAt(sb.length() - 1) == c)
                 sb.deleteCharAt(sb.length() - 1);
             else
                 sb.append(c);
