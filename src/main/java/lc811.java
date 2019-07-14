287241c06e2a267008029afc546d7da1640f57c0
sparky <yaoqiang.li.uw@gmail.com>
2019-07-14 16:26:39 +0800

    bug fix add the count

--- src/main/java/lc811.java
+++ src/main/java/lc811.java
@@ -3,9 +3,9 @@ import org.junit.Test;
 import java.util.*;
 
 public class lc811 {
-    private void add2Map(Map<String, Integer> d2c, String domain) {
-        d2c.computeIfPresent(domain, (k, v) -> v = v + 1);
-        d2c.putIfAbsent(domain, 1);
+    private void add2Map(Map<String, Integer> d2c, String domain, int count) {
+        d2c.computeIfPresent(domain, (k, v) -> v = v + count);
+        d2c.putIfAbsent(domain, count);
     }
 
     public List<String> subdomainVisits(String[] cpdomains) {
@@ -13,10 +13,11 @@ public class lc811 {
         for (int i = 0; i < cpdomains.length; i++) {
             String[] domain = cpdomains[i].trim().split("\\s+");
             if (domain.length != 2) continue;
-            add2Map(d2c, domain[1]);
+            int count = Integer.valueOf(domain[0]);
+            add2Map(d2c, domain[1], count);
             for (int j = 1; j < domain[1].length(); j++)
                 if (domain[1].charAt(j) == '.')
-                    add2Map(d2c, domain[1].substring(j + 1));
+                    add2Map(d2c, domain[1].substring(j + 1), count);
         }
         List<String> results = new ArrayList<>();
         Iterator it = d2c.entrySet().iterator();
