07b5ab9cd0497bae2055b0915d5bb2bf665c4878
sparky <yaoqiang.li.uw@gmail.com>
2019-07-20 22:20:14 +0800

    init commit for lc929

---
+++ src/main/java/lc929.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class lc929 {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> set = new HashSet<>();
+        for (String email : emails) {
+            StringBuilder sb = new StringBuilder();
+            boolean ignored = false;
+            boolean isDomained = false;
+            for (int i = 0; i < email.length(); i++) {
+                char c = email.charAt(i);
+                if (isDomained || c == '@') {
+                    sb.append(c);
+                    isDomained = true;
+                    continue;
+                }
+                if (ignored || c == '.') continue;
+                if (c == '+') {
+                    ignored = true;
+                    continue;
+                }
+                sb.append(c);
+
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+}
