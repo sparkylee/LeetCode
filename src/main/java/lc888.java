af0a9a5e462a286ceed3c51f155daeea3872ad46
sparky <yaoqiang.li.uw@gmail.com>
2019-07-28 15:06:47 +0800

    bug fix for exchange difference

--- src/main/java/lc888.java
+++ src/main/java/lc888.java
@@ -5,8 +5,11 @@ public class lc888 {
         Arrays.sort(A);
         Arrays.sort(B);
         int diff = Arrays.stream(A).sum() - Arrays.stream(B).sum();
+        if (diff % 2 != 0)
+            return null;
+        int delta = diff / 2;
         for (int a : A) {
-            int b = a - diff;
+            int b = a - delta;
             int index = Arrays.binarySearch(B, b);
             if (index >= 0) {
                 return new int[]{a, b};
