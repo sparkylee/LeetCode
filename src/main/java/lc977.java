a9b255a231c2dfa3c6fe3e8a6fa0784be67fa5e5
sparky <yaoqiang.li.uw@gmail.com>
2019-08-07 00:32:51 +0800

    init commit for lc977

---
+++ src/main/java/lc977.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+
+public class lc977 {
+    public int[] sortedSquares(int[] A) {
+        int n = -1, p = A.length;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] >= 0) {
+                n = i - 1;
+                p = i;
+                break;
+            }
+        }
+        for (int i = 0; i < A.length; i++) {
+            A[i] = A[i] * A[i];
+        }
+        int[] B = new int[A.length];
+        int k = 0;
+        while (true) {
+            int n2 = -1, p2 = -1;
+            if (n >= 0) n2 = A[n];
+            if (p < A.length) p2 = A[p];
+            if (n2 < 0 && p2 < 0) break;
+            if (p2 < 0 || p2 >= n2) {
+                B[k] = A[n];
+                k++;
+                n--;
+                continue;
+            }
+            if (n2 < 0 || n2 >= p2) {
+                B[k] = A[p];
+                k++;
+                p++;
+                continue;
+            }
+        }
+        return B;
+    }
+}
