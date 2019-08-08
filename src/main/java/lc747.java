711d2bed938bbb1fbb9676fdded5df469ff7fbff
sparky <yaoqiang.li.uw@gmail.com>
2019-08-08 22:16:14 +0800

    init commit for lc747

---
+++ src/main/java/lc747.java
@@ -0,0 +1,20 @@
+public class lc747 {
+    public int dominantIndex(int[] nums) {
+        int xi = -1, x = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (xi == -1 || nums[i] >= x) {
+                x = nums[i];
+                xi = i;
+            }
+        }
+        int yi = -1, y = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (yi != xi && (yi == -1 || nums[i] >= y)) {
+                y = nums[i];
+                yi = i;
+            }
+        }
+        if (yi == -1 || x >= y * 2) return xi;
+        return -1;
+    }
+}
