05e84a0d8459a7285ecbfe7e26f76734ddbb977f
sparky <yaoqiang.li.uw@gmail.com>
2019-08-09 00:12:19 +0800

    bug fixes

--- src/main/java/lc819.java
+++ src/main/java/lc819.java
@@ -7,33 +7,34 @@ public class lc819 {
     public void test() {
         String paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
         String[] banned = {"hit"};
-        mostCommonWord(paragraph, banned);
+        System.out.println(mostCommonWord(paragraph, banned));
     }
 
-    boolean isLetter(char c) {
+    boolean isLetter(int c) {
         return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
     }
 
     char toLowercase(char c) {
         if (c >= 'A' && c <= 'Z')
-            c = (char) (c - 'A');
+            c = (char) (c - 'A' + 'a');
         return c;
     }
 
     Map<String, Integer> paragraph2Map(String paragraph, Set<String> bannedSet) {
         StringBuilder sb = new StringBuilder();
         Map<String, Integer> wordMap = new HashMap<>();
-        for (int i = 0; i < paragraph.length(); i++) {
-            char c = paragraph.charAt(i);
+        for (int i = 0; i <= paragraph.length(); i++) {
+            int c = i < paragraph.length() ? paragraph.charAt(i) : -1;
             if (isLetter(c)) {
-                sb.append(toLowercase(c));
+                sb.append(toLowercase((char) c));
                 continue;
             }
             if (sb.length() == 0) continue;
             String x = sb.toString();
-            if (bannedSet.contains(x)) continue;
-            wordMap.computeIfPresent(x, (k, v) -> v = v + 1);
-            wordMap.putIfAbsent(x, 1);
+            if (!bannedSet.contains(x)) {
+                wordMap.computeIfPresent(x, (k, v) -> v = v + 1);
+                wordMap.putIfAbsent(x, 1);
+            }
             sb.setLength(0);
             sb.trimToSize();
         }
@@ -56,6 +57,7 @@ public class lc819 {
             Map.Entry<String, Integer> pair = (Map.Entry) it.next();
             if (pair.getValue() >= count) {
                 X = pair.getKey();
+                count = pair.getValue();
             }
         }
         return X;
