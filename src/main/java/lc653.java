52dbbdbdeb6610722e8915d68930b66d54994999
sparky <yaoqiang.li.uw@gmail.com>
2019-08-10 17:06:34 +0800

    init commit for lc653

---
+++ src/main/java/lc653.java
@@ -0,0 +1,31 @@
+public class lc653 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    private boolean findTarget(TreeNode node, TreeNode root, int k) {
+        if (node == null) return false;
+        if (findTarget(node.left, root, k)) return true;
+        int second = k - root.val;
+        if (root.val >= second) return false;
+        if (findSecond(root, second)) return true;
+        return findTarget(node.right, root, k);
+    }
+
+    private boolean findSecond(TreeNode root, int value) {
+        if (root == null) return false;
+        if (root.val < value && findSecond(root.left, value)) return true;
+        if (root.val == value) return true;
+        return findSecond(root.right, value);
+    }
+
+    public boolean findTarget(TreeNode root, int k) {
+        return findTarget(root, root, k);
+    }
+}
