98bb3b95e7c2c83e636ed0c6c69d87890557d34c
sparky <yaoqiang.li.uw@gmail.com>
2019-08-11 01:38:51 +0800

    init commit for lc1089

---
+++ src/main/java/lc1089.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class lc1089 {
+    public void duplicateZeros(int[] arr) {
+        int shift = 0;
+        int i = 0;
+        for (; i < arr.length; i++) {
+            if (i + shift == arr.length - 1)
+                break;
+            if (arr[i] == 0)
+                shift++;
+        }
+        for (; i >= 0; i--) {
+            arr[i + shift] = arr[i];
+            if (arr[i] == 0)
+                shift--;
+        }
+
+    }
+}
