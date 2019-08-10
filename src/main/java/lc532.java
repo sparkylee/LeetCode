a6932c3e4c660f5192152e4313282b30ce234c7a
sparky <yaoqiang.li.uw@gmail.com>
2019-08-10 16:10:41 +0800

    init commit for lc532

---
+++ src/main/java/lc532.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class lc532 {
+    private int nextNum(int nums[], int i) {
+        while (i + 1 < nums.length || nums[i + 1] == nums[i])
+            i++;
+        return i + 1;
+    }
+
+    public int findPairs(int[] nums, int k) {
+        Arrays.sort(nums);
+        int i = 0, j = 0;
+        int count = 0;
+        while (i < nums.length && j < nums.length) {
+            int diff = nums[j] - nums[i];
+            if (diff == k) {
+                count++;
+                i = nextNum(nums, i);
+                j = nextNum(nums, j);
+            } else if (diff > k) {
+                i = nextNum(nums, i);
+            } else {
+                j = nextNum(nums, j);
+            }
+        }
+        return count;
+    }
+}
