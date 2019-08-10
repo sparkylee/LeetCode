a862c01235ebee6cd3c2b39883c2327e38666066
sparky <yaoqiang.li.uw@gmail.com>
2019-08-10 19:30:21 +0800

    init commit for lc1051

---
+++ src/main/java/lc1051.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class lc1051 {
+    public int heightChecker(int[] heights) {
+        int[] heightsSorted = heights.clone();
+        Arrays.sort(heightsSorted);
+        int count = 0;
+        for (int i = 0; i < heights.length; i++)
+            count += heights[i] == heightsSorted[i] ? 0 : 1;
+        return count;
+    }
+}
