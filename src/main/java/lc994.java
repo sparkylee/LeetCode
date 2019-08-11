b691037766d8d8b4aa8e8f81e6b8cf1b91d7f487
sparky <yaoqiang.li.uw@gmail.com>
2019-08-11 15:50:03 +0800

    init commit for lc994

---
+++ src/main/java/lc994.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class lc994 {
+    class Coord {
+        int i, j;
+
+        Coord(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+    }
+
+    private boolean isFresh(int[][] grid, int i, int j) {
+        return i >= 0 && i < grid.length
+                && j >= 0 && j < grid[0].length && grid[i][j] == 1;
+    }
+
+    private void addAdjacentIfFresh(int[][] grid, List<Coord> rottens_new,
+                                    Coord orange, int delta_i, int delta_j) {
+        if (isFresh(grid, orange.i - 1, orange.j))
+            rottens_new.add(new Coord(orange.i + delta_i, orange.j + delta_j));
+    }
+
+    public int orangesRotting(int[][] grid) {
+        List<Coord> rottens = new ArrayList<>();
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[i].length; j++)
+                if (grid[i][j] == 2)
+                    rottens.add(new Coord(i, j));
+        int count = 0;
+        while (true) {
+            List<Coord> rottens_new = new ArrayList<>();
+            for (int i = 0; i < rottens.size(); i++) {
+                Coord orange = rottens.get(i);
+                addAdjacentIfFresh(grid, rottens_new, orange, -1, 0);
+                addAdjacentIfFresh(grid, rottens_new, orange, 1, 0);
+                addAdjacentIfFresh(grid, rottens_new, orange, 0, -1);
+                addAdjacentIfFresh(grid, rottens_new, orange, 0, 1);
+            }
+            count++;
+            if (rottens_new.isEmpty()) break;
+            rottens = rottens_new;
+        }
+        return count;
+    }
+}
