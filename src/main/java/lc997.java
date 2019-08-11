1f5dcfa3d59b57fdd61259430299c566af7b6326
sparky <yaoqiang.li.uw@gmail.com>
2019-08-11 16:25:45 +0800

    init commit for lc997

---
+++ src/main/java/lc997.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class lc997 {
+    public int findJudge(int N, int[][] trust) {
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++)
+            arr[i] = 0;
+        for (int i = 0; i < trust.length; i++) {
+            int truster = trust[i][0] - 1;
+            int trustee = trust[i][1] - 1;
+            arr[truster] = -1; // exclude
+            if (arr[trustee] != -1)
+                arr[trustee]++;
+        }
+        for (int i = 0; i < N; i++)
+            if (arr[i] == N - 1)
+                return i + 1;
+        return -1;
+    }
+}
