79b349bb368e3eeb218d2a052d4e59791092b05b
sparky <yaoqiang.li.uw@gmail.com>
2019-08-12 23:41:15 +0800

    bug fix for corner case K=0

--- src/main/java/lc989.java
+++ src/main/java/lc989.java
@@ -5,22 +5,22 @@ import java.util.List;
 public class lc989 {
     public List<Integer> addToArrayForm(int[] A, int K) {
         List<Integer> B = new ArrayList<>();
-        while (K > 0) {
+        do {
             int m = K % 10;
             K = K / 10;
             B.add(m);
-        }
+        } while (K > 0);
         int i = 0;
         int a = 0, b = 0, c = 0, sum = 0, s = 0;
         while (true) {
             int j = A.length - 1 - i;
             a = (j >= 0) ? A[j] : 0;
             b = (i < B.size()) ? B.get(i) : 0;
+            if (sum == 0) break;
             sum = a + b + c;
             s = sum % 10;
             c = sum / 10;
             if (i == B.size()) {
-                if (sum == 0) break;
                 B.add(s);
             }
             else
