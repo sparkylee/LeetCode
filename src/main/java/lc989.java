8d7888dd11358911114e2353bfe3f62fa61738e9
sparky <yaoqiang.li.uw@gmail.com>
2019-08-12 23:49:51 +0800

    fix big mistake

--- src/main/java/lc989.java
+++ src/main/java/lc989.java
@@ -10,16 +10,16 @@ public class lc989 {
             K = K / 10;
             B.add(m);
         } while (K > 0);
-        int i = 0;
         int a = 0, b = 0, c = 0, sum = 0, s = 0;
-        while (true) {
+        int len = Math.max(A.length, B.size());
+        for (int i = 0; i <= len; i++) {
             int j = A.length - 1 - i;
             a = (j >= 0) ? A[j] : 0;
             b = (i < B.size()) ? B.get(i) : 0;
             sum = a + b + c;
-            if (sum == 0) break;
             s = sum % 10;
             c = sum / 10;
+            if (i == len && sum == 0) break;
             if (i == B.size()) {
                 B.add(s);
             }
