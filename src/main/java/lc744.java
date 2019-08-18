91e332e5d308b552a170de4d2a521ff26e272bde
sparky <yaoqiang.li.uw@gmail.com>
2019-08-18 20:43:09 +0800

    init commit for lc744

---
+++ src/main/java/lc744.java
@@ -0,0 +1,18 @@
+public class lc744 {
+    private int searchLoc(char[] letters, int start, int end, char target) {
+        if (start + 1 >= end) {
+            return start;
+        }
+        int middle = (start + end);
+        if (letters[middle] <= target)
+            return searchLoc(letters, middle, end, target);
+        return searchLoc(letters, start, middle, target);
+    }
+
+    public char nextGreatestLetter(char[] letters, char target) {
+        if (target >= letters[letters.length - 1] || target < letters[0])
+            return letters[0];
+        int loc = searchLoc(letters, 0, letters.length - 1, target);
+        return letters[loc + 1];
+    }
+}
