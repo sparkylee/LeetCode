e389f11be71d5d4b5c8c44dd10e6b7c172c3ec43
sparky <yaoqiang.li.uw@gmail.com>
2019-08-19 01:18:51 +0800

    init commit for lc937

---
+++ src/main/java/lc937.java
@@ -0,0 +1,84 @@
+public class lc937 {
+    private int getStart(String log) {
+        int i = 0;
+        while (log.charAt(i) != ' ') i++;
+        return i + 1;
+    }
+
+    private boolean isDigitLog(String log, int start) {
+        char c = log.charAt(start);
+        return c >= '0' && c <= '9';
+    }
+
+    private int look4DigitLog(String[] logs, int[] starts, int i) {
+        while (i >= 0) {
+            if (isDigitLog(logs[i], starts[i]))
+                return i;
+            i--;
+        }
+        return i;
+    }
+
+    private void swap(String[] logs, int i, int j) {
+        String tmp = logs[i];
+        logs[i] = logs[j];
+        logs[j] = tmp;
+    }
+
+    private void sort(String[] logs, int[] starts, int end) {
+        for (int i = 0; i < end; i++) {
+            for (int j = i + 1; j < end; j++) {
+                if (compare(logs, starts, i, j))
+                    swap(logs, i, j);
+            }
+        }
+    }
+
+    private boolean compare(String[] logs, int[] starts, int i, int j) {
+        String li = logs[i];
+        int ii = starts[i];
+        String lj = logs[j];
+        int jj = starts[j];
+        int k = 0;
+        while (true) {
+            ii = starts[i] + k;
+            jj = starts[j] + k;
+            if (ii == li.length() - 1 && jj == lj.length() - 1)
+                break;
+            if (ii == li.length() - 1) return false;
+            if (jj == lj.length() - 1) return true;
+            if (li.charAt(ii) > lj.charAt(jj)) return true;
+            if (li.charAt(ii) < lj.charAt(jj)) return false;
+            k++;
+        }
+        k = 0;
+        while (true) {
+            char ci = li.charAt(k);
+            char cj = lj.charAt(k);
+            if (ci == ' ' && cj == ' ') return false;
+            if (cj == ' ') return true;
+            if (ci == ' ') return false;
+            if (ci > cj) return true;
+            if (ci < cj) return false;
+            k++;
+        }
+    }
+
+    public String[] reorderLogFiles(String[] logs) {
+        int[] starts = new int[logs.length];
+        for (int i = 0; i < logs.length; i++)
+            starts[i] = getStart(logs[i]);
+        int i = logs.length - 1;
+        while (i >= 0) {
+            int j = look4DigitLog(logs, starts, i);
+            if (j == -1)
+                break;
+            if (i != j)
+                swap(logs, i, j);
+            i--;
+        }
+        int end = i;
+        sort(logs, starts, end);
+        return logs;
+    }
+}
