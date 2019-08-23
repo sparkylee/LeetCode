ace00bbc9eed4aa93bceef6963b8abb13f3a185a
sparky <yaoqiang.li.uw@gmail.com>
2019-08-23 22:33:41 +0800

    init commit for lc701

---
+++ src/main/java/lc701.java
@@ -0,0 +1,21 @@
+public class lc701 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null)
+            return new TreeNode(val);
+        if (root.val > val)
+            root.left = insertIntoBST(root.left, val);
+        else
+            root.right = insertIntoBST(root.right, val);
+        return root;
+    }
+}
