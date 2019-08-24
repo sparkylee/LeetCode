5948117c8826b51c40bc57210692866f2c91890c
sparky <yaoqiang.li.uw@gmail.com>
2019-08-24 22:03:52 +0800

    init commit for lc739

---
+++ src/main/java/lc739.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+public class lc739 {
+    class Node {
+        int val;
+        int index;
+
+        Node(int val, int index) {
+            this.val = val;
+            this.index = index;
+        }
+    }
+
+    public int[] dailyTemperatures(int[] T) {
+        int[] warmer = new int[T.length];
+        Stack<Node> stack = new Stack<>();
+        for (int i = 0; i < T.length; i++) {
+            Node node = new Node(T[i], i);
+            while (!stack.empty()) {
+                Node top = stack.peek();
+                if (top.val < node.val) {
+                    warmer[top.index] = node.index - top.index;
+                    stack.pop();
+                }
+            }
+            stack.push(node);
+        }
+        return warmer;
+    }
+
+}
