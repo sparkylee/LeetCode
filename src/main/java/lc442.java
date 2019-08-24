ee8de1061d6a11582dc0af260c984681250e5958
sparky <yaoqiang.li.uw@gmail.com>
2019-08-24 18:50:36 +0800

    two round of scanning instead of one round

--- src/main/java/lc442.java
+++ src/main/java/lc442.java
@@ -7,15 +7,15 @@ public class lc442 {
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> results = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
-            if (i + 1 == nums[i]) continue;
             int v = nums[i];
-            if (nums[v - 1] == nums[i]) {
-                results.add(v);
-            } else {
-                int tmp = nums[v - 1];
-                nums[v - 1] = nums[i];
-                nums[i] = tmp;
-            }
+            if (i + 1 == v || nums[v - 1] == v) continue;
+            int tmp = nums[v - 1];
+            nums[v - 1] = nums[i];
+            nums[i] = tmp;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (i + 1 != nums[i])
+                results.add(nums[i]);
         }
         return results;
     }
