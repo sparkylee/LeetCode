bd0dd97d7ea2de3e24a190c7cc146dfa4399bc91
sparky <yaoqiang.li.uw@gmail.com>
2019-08-27 22:49:57 +0800

    add remaining code

---
+++ src/main/java/lc468.java
@@ -0,0 +1,64 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class lc468 {
+    private String[] resps = {"IPv4", "IPv6", "Neither"};
+
+    private int seekNextDot(String IP, int start) {
+        while (start < IP.length()) {
+            char c = IP.charAt(start);
+            if (c == '.')
+                return start;
+            if (c < '0' || c > '9') return -1;
+            start++;
+        }
+        return start;
+    }
+
+    boolean toIPV4(String IP) {
+        int start = 0, end = 0;
+        for (int k = 0; k < 4; k++) {
+            end = seekNextDot(IP, start);
+            if (end == -1 || start == end) return false;
+            String sub = IP.substring(start, end);
+            if (sub.length() > 1 && sub.charAt(0) == '0') return false;
+            int value = Integer.parseInt(sub);
+            if (value > 255) return false;
+            start = end + 1;
+        }
+        return end == IP.length();
+    }
+
+    private int seekNextColon(String IP, int start) {
+        while (start < IP.length()) {
+            char c = IP.charAt(start);
+            if (c == ':')
+                return start;
+            boolean valid = (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F');
+            if (!valid) return -1;
+            start++;
+        }
+        return start;
+    }
+
+    boolean toIPV6(String IP) {
+        int start = 0, end = 0;
+        for (int k = 0; k < 8; k++) {
+            end = seekNextColon(IP, start);
+            if (end == -1 || start == end) return false;
+            String sub = IP.substring(start, end);
+            if (sub.length() > 4) return false;
+            start = end + 1;
+        }
+        return end == IP.length();
+    }
+
+    public String validIPAddress(String IP) {
+        if (IP == null || "".equals(IP)) return resps[2];
+        if (toIPV4(IP)) return "IPv4";
+        if (toIPV6(IP)) return "IPv6";
+        return "Neither";
+    }
+}
