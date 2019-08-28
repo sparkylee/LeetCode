7e20e76617a4a8c142bc0d0e4d4327ee47c6f663
sparky <yaoqiang.li.uw@gmail.com>
2019-08-29 00:53:00 +0800

    improve the performance

--- src/main/java/lc322.java
+++ src/main/java/lc322.java
@@ -7,14 +7,24 @@ public class lc322 {
         t(new int[]{3, 2, 5}, 11);
         t(new int[]{3}, 4);
         t(new int[]{1}, 4);
+        t(new int[]{58, 92, 387, 421, 194, 208, 231}, 7798);
     }
 
     private void t(int[] coins, int amount) {
         System.out.println(coinChange(coins, amount));
     }
-    private int coinChange(int[] coins, int i, int amount) {
-        if (amount == 0)
+
+    private int countMin = -1;
+
+    private int coinChange(int[] coins, int i, int amount, int countPre) {
+        if (amount == 0) {
+            if (this.countMin == -1)
+                this.countMin = countPre;
+            else
+                this.countMin = Math.min(countPre, this.countMin);
             return 0;
+        }
+
         if (i >= coins.length || amount < 0) return -1;
         int j = 0;
         int countBest = -1;
@@ -22,7 +32,9 @@ public class lc322 {
             int extraction = coins[i] * j;
             int newAmount = amount - extraction;
             if (newAmount < 0) return countBest;
-            int leftover = coinChange(coins, i + 1, newAmount);
+            if (this.countMin >= 0 && countPre + j > this.countMin)
+                return countBest;
+            int leftover = coinChange(coins, i + 1, newAmount, countPre + j);
             if (leftover >= 0) {
                 int count = leftover + j;
                 if (countBest == -1)
@@ -36,7 +48,8 @@ public class lc322 {
     }
 
     public int coinChange(int[] coins, int amount) {
-        int count = coinChange(coins, 0, amount);
+        this.countMin = -1;
+        int count = coinChange(coins, 0, amount, 0);
         return count;
     }
 
