ece1ef6310d8835f2ede9b3eb2cab3b2f65ce073
sparky <yaoqiang.li.uw@gmail.com>
2019-08-29 00:29:21 +0800

    add more test cases and add corner case

--- src/main/java/lc322.java
+++ src/main/java/lc322.java
@@ -3,12 +3,19 @@ import org.junit.Test;
 public class lc322 {
     @Test
     public void test() {
-        coinChange(new int[]{1, 2, 5}, 11);
+        t(new int[]{1, 2, 5}, 11);
+        t(new int[]{3, 2, 5}, 11);
+        t(new int[]{3}, 4);
+        t(new int[]{1}, 4);
+    }
+
+    private void t(int[] coins, int amount) {
+        System.out.println(coinChange(coins, amount));
     }
     private int coinChange(int[] coins, int i, int amount) {
         if (amount == 0)
             return 0;
-        if (i >= coins.length) return -1;
+        if (i >= coins.length || amount < 0) return -1;
         int j = 0;
         int countBest = -1;
         while (true) {
@@ -23,6 +30,7 @@ public class lc322 {
                 else
                     countBest = Math.min(count, countBest);
             }
+            if (coins[i] == 0) return countBest;
             j++;
         }
     }
