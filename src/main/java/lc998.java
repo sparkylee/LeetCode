f1da2d33da1e6dd7646d5a044599f91f8901b0d1
sparky <yaoqiang.li.uw@gmail.com>
2019-08-29 23:38:47 +0800

    init commit for lc998

---
+++ src/main/java/lc998.java
@@ -0,0 +1,22 @@
+public class lc998 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if (root == null || root.val < val) {
+            TreeNode node = new TreeNode(val);
+            node.left = root;
+            return node;
+        }
+        TreeNode right = insertIntoMaxTree(root.right, val);
+        root.right = right;
+        return root;
+    }
+}
