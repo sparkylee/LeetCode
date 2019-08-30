8b2d4425255b73e55ca51793a0f47d66b6f9ede3
sparky <yaoqiang.li.uw@gmail.com>
2019-08-30 23:41:51 +0800

    final optimization

--- src/main/java/lc322.java
+++ src/main/java/lc322.java
@@ -14,8 +14,9 @@ public class lc322 {
         t(new int[]{58, 92, 387, 421, 194, 208, 231}, 7798);
         t(new int[]{342, 268, 284, 65, 217, 461, 245, 249, 106}, 9278);
         t(new int[]{120, 6, 320, 300, 100, 192, 212, 89, 106, 461}, 8332);
+        t(new int[]{496, 154, 300, 357, 327, 248, 201, 341, 145, 248, 316}, 6776);
         long t2 = System.currentTimeMillis();
-        System.out.println(t2 - t1);
+        System.out.println("time used: " + (t2 - t1));
     }
 
     private void t(int[] coins, int amount) {
@@ -33,24 +34,28 @@ public class lc322 {
             return 0;
         }
 
-        if (i >= coins.length || amount < 0) return -1;
-        int j = 0;
-        if (i == coins.length - 1) {
-            if (coins[i] == 0) return -1;
-            j = amount / coins[i];
+        if (i >= coins.length || i < 0 || amount < 0) return -1;
+        if (coins[i] == 0) return -1;
+        int j = amount / coins[i];
+        if (i == 0) {
             int modulus = amount % coins[i];
             if (modulus != 0) return -1;
+            if (this.countMin == -1)
+                this.countMin = countPre + j;
+            else
+                this.countMin = Math.min(countPre + j, this.countMin);
+            return j;
         }
         int countBest = -1;
         int extraction = 0;
-        while (true) {
+        while (j >= 0) {
             extraction = coins[i] * j;
             int newAmount = amount - extraction;
             if (newAmount < 0) return countBest;
             if (this.countMin >= 0 && countPre + j > this.countMin)
                 return countBest;
             if (countBest >= 0 && j >= countBest) return countBest;
-            int leftover = coinChange(coins, i + 1, newAmount, countPre + j);
+            int leftover = coinChange(coins, i - 1, newAmount, countPre + j);
             if (leftover >= 0) {
                 int count = leftover + j;
                 if (countBest == -1)
@@ -59,14 +64,15 @@ public class lc322 {
                     countBest = Math.min(count, countBest);
             }
             if (coins[i] == 0) return countBest;
-            j++;
+            j--;
         }
+        return countBest;
     }
 
     public int coinChange(int[] coins, int amount) {
         Arrays.sort(coins);
         this.countMin = -1;
-        int count = coinChange(coins, 0, amount, 0);
+        int count = coinChange(coins, coins.length - 1, amount, 0);
 
         return count;
     }
