fbc99a9e609aa56f099bd504ce74b42fdf644a60
sparky <yaoqiang.li.uw@gmail.com>
2019-09-01 00:36:29 +0800

    fix the bug of missing case

--- src/main/java/lc5xx/lc526.java
+++ src/main/java/lc5xx/lc526.java
@@ -7,20 +7,17 @@ public class lc526 {
         arr[j] = tmp;
     }
 
-    private boolean isFitted(int[] arr, int i) {
-        return (i + 1) % arr[i] == 0 || arr[i] % (i + 1) == 0;
+    private boolean isFitted(int x, int y) {
+        return (y % x == 0) || (x % y == 0);
     }
 
     private int countArrangement(int[] arr, int start) {
         if (start >= arr.length) return 1;
         int count4level = 0;
         for (int i = start; i < arr.length; i++) {
+            if (!isFitted(arr[i], start + 1)) continue;
             swap(arr, start, i);
-            boolean first = isFitted(arr, start);
-            boolean second = isFitted(arr, i);
-            if (first && second) {
-                count4level += countArrangement(arr, start + 1);
-            }
+            count4level += countArrangement(arr, start + 1);
             swap(arr, start, i);
         }
         return count4level;
