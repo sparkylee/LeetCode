bb44409ab4cfefe91857824ba2cdf60f6b6133a0
sparky <yaoqiang.li.uw@gmail.com>
2019-08-31 16:40:48 +0800

    init commit for lc451

---
+++ src/main/java/lc451.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class lc451 {
+    class CharCount {
+        char c;
+        int a;
+
+        CharCount(char c, int a) {
+            this.c = c;
+            this.a = a;
+        }
+    }
+
+    class CountComparator implements Comparator<CharCount> {
+        @Override
+        public int compare(CharCount x, CharCount y) {
+            return y.a - x.a;
+        }
+    }
+
+    public String frequencySort(String s) {
+        int[] charCnt = new int[256];
+        for (int i = 0; i < s.length(); i++)
+            charCnt[s.charAt(i)]++;
+        List<CharCount> ccList = new ArrayList<>();
+        for (int i = 0; i < 256; i++)
+            if (charCnt[i] != 0)
+                ccList.add(new CharCount((char) i, charCnt[i]));
+        ccList.sort(new CountComparator());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ccList.size(); i++) {
+            CharCount cc = ccList.get(i);
+            for (int j = 0; j < cc.a; j++)
+                sb.append(cc.c);
+        }
+        return sb.toString();
+    }
+}
