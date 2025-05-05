42330e2aac5b14fc8e17dc9b6aabc10b3906cdfc
Sparky Li <sparky.li@lotusflare.com>
2025-05-05 14:14:48 -0400

    0008

---
+++ solutions/0008. String to Integer (atoi)/0008.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int add10(int num, int digi) //y = num*10+digi;
+	{
+		if (num >(INT_MAX / 10))
+			return INT_MAX;
+		if (num <(INT_MIN / 10))
+			return INT_MIN;
+		int lhs = num * 10;
+		int rhs = digi;
+		if (lhs >= 0 && rhs >= 0) {
+			if (INT_MAX - lhs <= rhs) {
+				return INT_MAX;
+			}
+		}
+		else if (lhs < 0 && rhs < 0) {
+			if (lhs <= INT_MIN - rhs) {
+				return INT_MIN;
+			}
+		}
+		return lhs + rhs;
+	}
+
+    int myAtoi(string str) {
+        if (str.empty())
+			return 0;
+		int Sign = 1;
+		int Num = 0;
+		int i = 0;
+		while (isspace(str[i]))
+			i++;
+		if (str[i] == '-')
+		{
+			Sign = -1;
+			i++;
+		}
+		else if (str[i] == '+')
+		{
+			Sign = 1;
+			i++;
+		}
+		for (; i < str.length(); i++)
+		{
+			if (str[i] >= '0' && str[i] <= '9')
+			{
+				int nn = (int)((str[i] - '0'));
+				Num = add10(Num, nn*Sign);
+				//Num = Num * 10 + nn;
+			}
+			else
+				break;
+		}
+		return Num;
+    }
+};
