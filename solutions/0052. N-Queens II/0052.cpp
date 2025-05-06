d600423ffb97245c23988d3a8cba6d8bc17b596d
Sparky Li <sparky.li@lotusflare.com>
2025-05-06 11:27:18 -0400

    0052

---
+++ solutions/0052. N-Queens II/0052.cpp
@@ -0,0 +1,54 @@
+class Solution {
+    int nn = 0;
+    bool isSafeGrid(vector<string> & ve,int k, int i)
+    {
+        for(int y = k-1;y>=0;y--)
+        {
+            if(ve[y][i]=='Q') return false;
+        }
+        int d = 1;
+        while(k-d>=0 && i-d>=0)
+        {
+            if(ve[k-d][i-d]=='Q') return false;
+            d++;
+        }
+        d = 1;
+        while(k-d>=0 && i+d < ve.size())
+        {
+            if(ve[k-d][i+d]=='Q') return false;
+            d++;
+        }
+
+        return true;
+    }
+    void searchNQueens(vector<string> & ve,int k)
+    {
+        if(k==ve.size())
+        {
+            nn ++;
+            return;
+        }
+
+        for(int i=0;i<ve.size();i++)
+        {
+            if(isSafeGrid(ve,k,i))
+            {
+                ve[k][i]='Q';
+                searchNQueens(ve,k+1);
+                ve[k][i]='.';
+            }
+        }
+        return;
+    }
+public:
+    int totalNQueens(int n) {
+         vector<string> ve;
+        for(int i=0; i < n; i++)
+        {
+            string s(n,'.');
+            ve.push_back(s);
+        }
+        searchNQueens(ve,0);
+        return nn;
+    }
+};
