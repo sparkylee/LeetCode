9459a5a5da241cdf42d6634a557f720ff7e15eef
Sparky Li <sparky.li@lotusflare.com>
2025-05-06 10:54:45 -0400

    0046

---
+++ solutions/0046. Permutations/0046.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    vector<vector<int>> vve;
+    void permuteRecursive(vector<int>& nums,int k)
+    {
+        if(k == nums.size())
+        {
+            vve.push_back(nums);
+            return;
+        }
+        for(int i = k; i < nums.size(); i++)
+        {
+            int tmp = nums[k];
+            nums[k] = nums[i];
+            nums[i] = tmp;
+            permuteRecursive(nums,k+1);
+            nums[i] = nums[k];
+            nums[k] = tmp;
+
+        }
+        return;
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        permuteRecursive(nums,0);
+        return vve;
+    }
+};
