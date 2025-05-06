c30426d404cffe8bd81fef196134adf0e3155c01
Sparky Li <sparky.li@lotusflare.com>
2025-05-06 10:09:40 -0400

    0028

---
+++ solutions/0028. Find the Index of the First Occurrence in a String/0028.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.length() == 0)
+			return 0;
+		if ( needle.length() > haystack.length())
+			return -1;
+		int i = 0;
+		while (i + needle.length() <= haystack.length())
+		{
+			bool matched = true;
+			for (int j = 0; j < needle.length(); j++)
+			{
+				if (needle[j] != haystack[i + j])
+				{
+					matched = false;
+					break;
+				}
+			}
+			if (matched)
+				return i;
+			else
+			{
+				i++;
+			}
+		}
+		return -1;
+    }
+};
