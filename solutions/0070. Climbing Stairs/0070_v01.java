5bfa38e1401a00f37b20945b37b64a689a25153b
Sparky Li <sparky.li@lotusflare.com>
2025-05-07 11:38:45 -0400

    update 0070

---
+++ solutions/0070. Climbing Stairs/0070_v01.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int climbStairs(int n) {
+        int []  nums    = new int[n+1];
+        nums[0] = 1;
+        nums[1] = 1;
+        for(int i=2;i<n+1;i++){
+            nums[i] = nums[i-1]+nums[i-2];
+        }
+        return nums[n];
+    }
+}
