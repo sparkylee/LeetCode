1151728cbbdfc035fc6f8bfe39ec0d95d21abd7b
Sparky Li <sparky.li@lotusflare.com>
2025-05-08 15:53:41 -0400

    update 0122

---
+++ solutions/0122. Best Time to Buy and Sell Stock II/0122.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+       int bp = 0, pp = 1;
+		bp = 0;
+		pp = 0;
+		if (prices.empty())
+			return 0;
+		int profit = 0;
+		while ((bp < prices.size() - 1) && (pp < prices.size()))
+		{
+			bp = searchBottom(prices, pp);
+			pp = searchPeak(prices, bp);
+			profit += prices[pp] - prices[bp];
+		}
+		return profit;
+    }
+private:
+	int searchPeak(vector<int>& prices,int start)
+	{
+		while ((start + 1 < prices.size()) && (prices[start] <= prices[start + 1]))
+			start++;
+		return start;
+	}
+	int searchBottom(vector<int>& prices, int start)
+	{
+		while ((start + 1 < prices.size()) && (prices[start] >= prices[start + 1]))
+			start++;
+		return start;
+	}
+};
