5a1aec4a851b53755b35a47e0ef6d07648cef6e9
Sparky Li <sparky.li@lotusflare.com>
2025-05-08 15:42:36 -0400

    update 0114

---
+++ solutions/0114. Flatten Binary Tree to Linked List/0114_v01.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private TreeNode flattenRecursive(TreeNode root)
+    {
+        if(root==null) return null;
+        TreeNode root_tail   = root;
+        TreeNode left_tail  = flattenRecursive(root.left);
+        TreeNode right_tail = flattenRecursive(root.right);
+        TreeNode tmp = root.right;
+        if(root.left!=null)
+        {
+            root.right = root.left;
+            root.left = null;
+            root_tail = left_tail;
+
+        }
+        root_tail.right = tmp;
+        if(tmp!=null)
+        {
+            root_tail = right_tail;
+        }
+        return root_tail;
+    }
+
+    public void flatten(TreeNode root) {
+        flattenRecursive(root);
+    }
+}
