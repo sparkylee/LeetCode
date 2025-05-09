d98418670a8b402b1afc50d3f7a43d8c1baf675a
Sparky Li <sparky.li@lotusflare.com>
2025-05-09 11:53:20 -0400

    update 0151

---
+++ solutions/0151. Reverse Words in a String/0151_v01.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public String reverseWords(String s) {
+        String [] strArray = s.trim().split("\\s+" );
+        StringBuffer sb =  new StringBuffer();
+        for(int i=strArray.length-1;i>=0;i--)
+        {
+            sb.append(strArray[i]);
+            if(i!=0)
+                sb.append(' ');
+        }
+        return sb.toString();
+    }
+}
